0f865cd4baedde6169d109037b3c518a9ac33a44
yakult1995 <destin.tax@outlook.jp>
2019-02-20 01:18:04 -0700

    refactering

--- main.py
+++ main.py
@@ -6,14 +6,10 @@ import os
 from datetime import datetime
 from os.path import join, dirname
 from dotenv import load_dotenv
+from sqlalchemy import create_engine
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
-
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm.exc import NoResultFound
 
 # MySQL接続
 mysql_info = 'mysql+pymysql://' + os.environ['MYSQL_USER'] + ':' + os.environ['MYSQL_PASSWORD'] + '@' + os.environ['MYSQL_HOST'] + '/' + os.environ['MYSQL_DATABASE'] + '?charset=utf8'
@@ -21,7 +17,7 @@ engine = create_engine(mysql_info, echo=True)
 
 
 # API用のAttachment
-def make_attachement(article):
+def make_attachment(article):
     fields = []
     field = {
         'title': article['resolved_title'],
@@ -35,7 +31,7 @@ def make_attachement(article):
         "fallback": "Pocket新規アイテム",
         "color": "#dd0000",
         "fields": fields,
-        "footer": "データ取得日次: " + datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        "footer": datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     }
     slack_params = {
         'channel': os.environ['CHANNEL_ID'],
@@ -69,16 +65,13 @@ payload = {
 r = requests.post('https://getpocket.com/v3/get', data=payload)
 articles = r.json()['list']
 
-
-# MySQLとデータの整合性
-rows = engine.execute('SELECT * FROM article')
-for row in rows:
-    print(row.article_id)
-
 # PocketのIDだけをリスト化
 pocket_id_list = []
 for index in articles:
     pocket_id_list.append(int(index))
+
+# MySQLとデータの整合性
+rows = engine.execute('SELECT * FROM article')
 
 # MySQLからデータ取得
 rows = engine.execute('SELECT * FROM article')
@@ -90,12 +83,8 @@ for row in rows:
 diff_id_list = list(set(pocket_id_list) - set(database_id_list))
 
 # 差分の記事を投稿
-for id in diff_id_list:
+ins = "INSERT INTO article (article_id, article_url, article_title) VALUES (%s, %s, %s)"
+for target_id in diff_id_list:
     # リストにあるものをDATABASEに登録
-    ins = "INSERT INTO article (article_id, article_url, article_title) VALUES (%s, %s, %s)"
-    engine.execute(ins, id, articles[str(id)]['resolved_url'], articles[str(id)]['resolved_title'])
-    upload_message_by_api(make_attachement(articles[str(id)]))
-
-
-for index in articles:
-    print(index, articles[index]['resolved_title'], articles[index]['resolved_url'])
+    engine.execute(ins, target_id, articles[str(target_id)]['resolved_url'], articles[str(target_id)]['resolved_title'])
+    upload_message_by_api(make_attachment(articles[str(target_id)]))
